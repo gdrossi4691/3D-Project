99b03d41d3c4afd760c8a85a3b6f524f5196ddbe
gdrossi4691 <gdadm46@gmail.com>
2014-11-14 20:21:40 -0800

    Visibility function

--- SHADOWS_HW7/GzPCFVisibilityFn.cpp
+++ SHADOWS_HW7/GzPCFVisibilityFn.cpp
@@ -1,8 +1,28 @@
 #include "stdafx.h"
 #include "rend.h"
+#define	ARRAY(x,y)	(x+(y*display->xres))
 
 float GzPCFVisibilityFn(float x, float y, float z, GzRender* map, GzLight* light) {
 	// TODO:
+	//Transforming this pixel value to screen space
+	float screenX, screenY, screenZ, screenW;
+	multiplyMatrixByVector(x, y, z,map->Ximage[map->matlevel],&screenX,&screenY,&screenZ,&screenW);
 
-	return 1.0;
+	//Get the Z values for neighbouring pixels using display->fbuf and compare it with transformed pixel Z-values
+	float visibility;
+	GzDisplay* display = (GzDisplay*)map->display;
+	//Checking for screen space bounds
+	if(screenX >= 0 && screenX < display->xres && screenY >= 0 && screenY < display->yres){
+		GzDepth neighbourZ = display->fbuf[ARRAY((int)screenX,(int)screenY)].z;
+		if(screenZ <= neighbourZ){
+			visibility = 1.0;
+		}
+		else{
+			visibility = 0.0;
+		}
+	}
+	else if((screenX < 0) || (screenX > display->xres) || (screenY < 0) || (screenY > display->yres)){
+		visibility = 1.0;
+	}
+	return visibility;
 }
